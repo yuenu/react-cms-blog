a506a45bd731168f893ce779bb4d92697caa4702
Noah <yuenuni@hotmail.com>
2021-12-07 14:34:49 +0800

    feat: author component

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
 
-function Author() {
+function Author({ author }) {
   return (
-    <div>
-      Author
+    <div className="relative p-12 mt-20 mb-8 text-center bg-black rounded-lg bg-opacity-20">
+      <div className="absolute left-0 right-0 -top-14">
+        <Image
+          src={author.photo.url}
+          alt={author.name}
+          unoptimized
+          width="100px"
+          height="100px"
+          className="align-middle rounded-full"
+        />
+      </div>
+        <h3 className="my-4 text-xl font-bold text-white">{author.name}</h3>
+        <p className="text-lg text-white">{author.bio}</p>
     </div>
   )
 }
